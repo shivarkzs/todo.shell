9a2781740f42bed56566440601d8452bcf024360
shivark <mr.shivark@gmail.com>
2021-05-11 16:47:11 +0530

    users

---
+++ components/users.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source components/common.sh
+OS_PREREQ
+
+Head "Installing Dependency"2
+apt install openjdk-8-jdk -y &>>$LOG && apt install maven -y &>>$LOG
+stat $?
+
+
+Head "Downloading the component"
+cd /root/
+git clone https://github.com/shivarkzs/users.git &>>$LOG && cd users
+rm -rf /etc/systemd/system/users.service
+
+Head "Updating Endpoints"
+mv systemd.service /etc/systemd/system/users.service
+sed -i -e "s/Login_Endpoint/login.shivark.online/" /etc/systemd/system/users.service
+stat $?
+
+Head "Building the Code"
+mvn clean &>>$LOG && mvn clean package &>>$LOG
+stat $?
+
+Head "Starting the Service"
+systemctl daemon-reload &>>$LOG && systemctl start users && systemctl enable users &>>$LOG
+systemctl status users
