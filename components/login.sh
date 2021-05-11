8fdd3b77339f341447df92ff7ae7d8bcad4ec7d5
shivark <mr.shivark@gmail.com>
2021-05-11 15:35:55 +0530

    login

---
+++ components/login.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source components/common.sh
+OS_PREREQ
+
+Head "Installing Dependency"
+apt install golang -y &>>$LOG
+Stat $?
+
+Head "Downloading Component"
+
+cd /root/
+rm -rf login
+git clone https://github.com/shivarkzs/login.git&>>$LOG && cd login
+
+
+Head "Building Package"
+go build &>>$LOG
+
+Head "Get dependencies"
+go get github.com/dgrijalva/jwt-go &>>$LOG && go get github.com/labstack/echo &>>$LOG && go get github.com/labstack/echo/middleware &>>$LOG && go get github.com/labstack/gommon/log &>>$LOG && go get github.com/openzipkin/zipkin-go &>>$LOG && go get github.com/openzipkin/zipkin-go/middleware/http &>>$LOG && go get github.com/openzipkin/zipkin-go/reporter/http &>>$LOG
+Stat $?
+Head "Again Build"
+go build main.go user.go tracing.go &>>$LOG
+
+rm -rf /etc/systemd/system/login.service
+mv systemd.service /etc/systemd/system/login.service
+sed -i -e "s/USERS_ENDPOINT/users.shivark.online/" /etc/systemd/system/login.service
+
+Head "Restarting Services"
+systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
+systemctl status login
