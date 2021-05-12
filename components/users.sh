45ece54642842408c3c9c0bdc5c15795a4fc8f99
shivark <mr.shivark@gmail.com>
2021-05-12 13:28:18 +0530

    2

--- components/users.sh
+++ components/users.sh
@@ -13,15 +13,15 @@ cd /root/
 git clone https://github.com/shivarkzs/users.git &>>$LOG && cd users
 #rm -rf /etc/systemd/system/users.service
 
-Head "Updating Endpoints"
-mv systemd.service /etc/systemd/system/users.service
-sed -i -e "s/Login_Endpoint/login.shivark.online/" /etc/systemd/system/users.service
-stat $?
+
 
 Head "Building the Code"
 mvn clean &>>$LOG && mvn clean package &>>$LOG
 stat $?
-
+Head "Updating Endpoints"
+mv /root/users/systemd.service /etc/systemd/system/users.service
+sed -i -e "s/LOGIN_ENDPOINT/login.shivark.online/" /etc/systemd/system/users.service
+stat $?
 Head "Starting the Service"
 systemctl daemon-reload &>>$LOG && systemctl start users && systemctl enable users &>>$LOG
 systemctl status users
